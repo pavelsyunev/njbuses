a756a0975127294e72dbbe8a015d0073e223721b
pavelsyunev <iloha@3541516354>
2022-02-21 20:23:17 -0500

    Added favorite stops

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from parser import get_bus_schedule
 
 app = Flask(__name__)
 
 
+# app.config['SECRET_KEY'] = 'qasr4r4ed45tfr237787hh343ghgbfgbfgbrt'
+
+
 @app.route('/')
 def index():
     bus_schedule = get_bus_schedule()
-    return render_template("index.html", bus_schedule=bus_schedule)
+    return render_template("index.html", bus_schedule=bus_schedule, bus_stop_name='🚏- Boulevard EAST 66th ST')
+
+
+@app.route('/<int:bus_stop_id>')
+def bus_stop(bus_stop_id):
+    bus_schedule = get_bus_schedule(bus_stop_id)
+    if bus_stop_id == 21890:
+        bus_stop_name = '🚏- Boulevard EAST 66th ST'
+    elif bus_stop_id == 32084:
+        bus_stop_name = '🚏- Willow AVE 15th ST'
+    elif bus_stop_id == 26229:
+        bus_stop_name = '🚏- PORT AUTHORITY BUS TERMINAL'
+    else:
+        bus_stop_name = ''
+    return render_template("index.html", bus_schedule=bus_schedule, bus_stop_name=bus_stop_name)
 
 
 if __name__ == "__main__":
--- parser.py
+++ parser.py
@@ -5,8 +5,18 @@ session = HTMLSession()
 # Selected Direction: New York
 # Selected Stop: BOULEVARD EAST + 66TH ST. (New York)
 # Selected Stop: 21890
-def get_bus_schedule():
-    r = session.get('https://mybusnow.njtransit.com/bustime/wireless/html/eta.jsp?route=165&direction=New+York&id=21890&showAllBusses=on')
+def get_bus_schedule(id='21890'):
+    print(id)
+    if id == '21890':
+        url = f'https://mybusnow.njtransit.com/bustime/wireless/html/eta.jsp?route=165&direction=New+York&id={id}&showAllBusses=on'
+    elif id == '32084':
+        url = f'https://mybusnow.njtransit.com/bustime/wireless/html/eta.jsp?route=126&direction=New+York&id={id}&showAllBusses=on'
+    elif id == '26229':
+        url = f'https://mybusnow.njtransit.com/bustime/wireless/html/eta.jsp?route=165&direction=Westwood&id={id}&showAllBusses=on'
+    else:
+        url = f'https://mybusnow.njtransit.com/bustime/wireless/html/eta.jsp?route=165&direction=New+York&id={id}&showAllBusses=on'
+    print(url)
+    r = session.get(url)
     res = r.html.find('.larger')
     res_arr = []
     for i in range(0, len(res)):
@@ -16,7 +26,6 @@ def get_bus_schedule():
             index = len(res_arr) - 1
             res_arr[index] = res_arr[index] + ' - ' + res[i].text.replace(u'\xa0', ' ')
 
-    # print('resArr', res_arr)
     bus_schedule = res_arr
     return bus_schedule
 
